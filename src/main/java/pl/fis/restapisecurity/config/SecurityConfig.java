d60afc6a998c3871193c2c2ca67b60e8f793bc58
wiktfic226 <wiktoriafica00@gmail.com>
2022-07-20 13:06:17 +0200

    feat: add jwt basic configuration

--- src/main/java/pl/fis/restapisecurity/config/SecurityConfig.java
+++ src/main/java/pl/fis/restapisecurity/config/SecurityConfig.java
@@ -5,12 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import pl.fis.restapisecurity.enums.Permission;
-import pl.fis.restapisecurity.enums.UserRole;
+import pl.fis.restapisecurity.jwt.JwtTokenVerifier;
+import pl.fis.restapisecurity.jwt.JwtUsernameAndPasswordAuthenticationFilter;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -20,10 +22,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .cors().and().csrf().disable()
-                .authorizeRequests()
-                .anyRequest().authenticated()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .httpBasic();
+                .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager()))
+                .addFilterAfter(new JwtTokenVerifier(), JwtUsernameAndPasswordAuthenticationFilter.class)
+                .authorizeRequests()
+                .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+                .anyRequest().authenticated();
     }
 
     @Bean
